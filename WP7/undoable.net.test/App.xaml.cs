f527f3b2bd94faee8b0a27c0a0ebba31476e86fe
Ollie Riches <ollie.riches@btinternet.com>
2012-02-27 22:46:29 +0000

    Removed icons from project

--- WP7/undoable.net.test/App.xaml.cs
+++ WP7/undoable.net.test/App.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-
-namespace undoable.net.test
+﻿namespace undoable.net.test
 {
+    using System.Windows;
+    using System.Windows.Navigation;
+    using Microsoft.Phone.Controls;
+    using Microsoft.Phone.Shell;
+
     public partial class App : Application
     {
         /// <summary>
--- WP7/undoable.net.test/MainPage.xaml.cs
+++ WP7/undoable.net.test/MainPage.xaml.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
-
-namespace undoable.net.test
+﻿namespace undoable.net.test
 {
+    using Microsoft.Phone.Controls;
+
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
         public MainPage()
         {
             InitializeComponent();
